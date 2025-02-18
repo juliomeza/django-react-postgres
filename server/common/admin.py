5aff95bc775186aba208a5741c5bf3ce26d4ca2d
Julio Meza <juliomeza@gmail.com>
2025-02-18 11:34:22 -0500

    backend tiene la base pero falta mejorarlo

--- server/common/admin.py
+++ server/common/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Status
 
-# Register your models here.
+@admin.register(Status)
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'code')
+    search_fields = ('name', 'description')
