d8a92c8575e2c0338d2cddfa8e5fd1f60058bbbf
Julio Meza <juliomeza@gmail.com>
2025-02-18 15:35:48 -0500

    Mejorando el formulario de django para crear usuario

--- server/users/admin.py
+++ server/users/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser, Role
 
 @admin.register(Role)
@@ -7,6 +8,26 @@ class RoleAdmin(admin.ModelAdmin):
     search_fields = ('role_name',)
 
 @admin.register(CustomUser)
-class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ('email', 'first_name', 'last_name', 'role')
+class CustomUserAdmin(UserAdmin):  # Usa UserAdmin en lugar de ModelAdmin
+    list_display = ('email', 'first_name', 'last_name', 'role', 'project', 'is_staff', 'is_active')
     search_fields = ('email', 'first_name', 'last_name')
+    list_filter = ()
+
+    # Orden y organización de los campos en el formulario de edición
+    fieldsets = (
+        ("Personal Information", {'fields': ('first_name', 'last_name', 'email', 'password')}),
+        ("User Details", {'fields': ('username', 'role', 'project')}),
+        ("Permissions", {'fields': ('is_staff', 'is_active', 'is_superuser', 'user_permissions')}),
+        ("Fechas Importantes", {'fields': ('last_login', 'date_joined')}),
+    )
+
+    # Orden en el formulario de creación de usuario en Django Admin
+    add_fieldsets = (
+        ("Personal Information", {
+            'classes': ('wide',),
+            'fields': ('email', 'first_name', 'last_name', 'username', 'password1', 'password2', 'role', 'project'),
+        }),
+        ("Permissions", {'fields': ('is_staff', 'is_active')}),
+    )
+
+    ordering = ('email',)
