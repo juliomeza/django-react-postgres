88082b4409c785730ee8423e7560b832a3776374
Julio Meza <juliomeza@gmail.com>
2025-02-18 11:09:58 -0500

    cambie url que use auth en lugar de users back y front

--- server/config/urls.py
+++ server/config/urls.py
@@ -30,6 +30,6 @@ router.register(r'order-lines', OrderLineViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/users/', include('users.urls')),
+    path('api/auth/', include('users.urls')),
     path('api/', include(router.urls)),
 ]
